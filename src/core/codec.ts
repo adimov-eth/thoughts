c0a99c052a041570c712fae7e7151ac75fb6fd21
adimov-eth <140806326+adimov-eth@users.noreply.github.com>
2025-07-02 23:24:40 +0700

    Simplify RLP codec wrapper

--- src/core/codec.ts
+++ src/core/codec.ts
@@ -1,9 +1,6 @@
 import { encode as rlpEncode, decode as rlpDecode } from 'rlp';
-import { concatBytes } from '@noble/hashes/utils';
-
-export const encodeBytes = (...chunks: Uint8Array[]) => concatBytes(...chunks);
 
 export const rlp = {
   enc: (v: unknown): Uint8Array => new Uint8Array(rlpEncode(v as any) as Buffer),
-  dec: (b: Uint8Array): any => rlpDecode(Buffer.from(b)),
+  dec: (b: Uint8Array): unknown => rlpDecode(Buffer.from(b)),
 };
